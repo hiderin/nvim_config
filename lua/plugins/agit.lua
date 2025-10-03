6296156809b8b281dca3c6ed459fafe1e9eda279
Naohide Hashimoto <hashimoto@akigh.co.jp>
2025-10-03 09:12:32 +0900

    文字化けのコメントを削除

--- lua/plugins/agit.lua
+++ lua/plugins/agit.lua
@@ -2,17 +2,15 @@ return {
 	"cohama/agit.vim",
 	config = function()
 		-- Agit.vim
-		-- カーソル移動で一覧と差分を更新させない
 		vim.g.agit_enable_auto_show_commit = 0
 
-		-- autocmd グループ
+		-- autocmd
 		vim.api.nvim_create_augroup("agit_rc", { clear = true })
 
 		vim.api.nvim_create_autocmd("FileType", {
 			group = "agit_rc",
 			pattern = "agit",
 			callback = function()
-				-- デフォルトのキーマッピングを変更
 				vim.keymap.set("n", "cp", "<Plug>(agit-git-cherry-pick)", { buffer = true })
 			end,
 		})
--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,7 +1,7 @@
 return {
   "tpope/vim-fugitive",
   config = function()
-	-- fugitive.vimのgit commitの画面をutf-8に変換してから閉じる
+	-- fugitive.vim
 	vim.keymap.set("n",
 	"<Leader>cmt",
 	":set fenc=utf-8<CR>:wq!<CR>",
